4ee1ea4c9e0956d9a93c212e87300bb616787d4a
Allie251B <ality.bamb@hotmail.com>
2023-02-12 17:52:11 +0200

    removing images that are not used

--- backgrounds/sprites/enemies_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="enemies_spritesheet" tilewidth="32" tileheight="32" tilecount="44" columns="11">
- <image source="../ground/Enemies/enemies_spritesheet.png" width="353" height="153"/>
-</tileset>
